44b6740fe7fcd796f23da7b99ffb792766047fff
Haeji Jung <gpwl0709@korea.ac.kr>
2023-12-07 20:54:37 +0000

    config for one epoch exp

--- src/config.py
+++ src/config.py
@@ -1,17 +1,18 @@
 config = {
-    "run_name": "bart-large-debug",
+    "run_name": "bart-large-one_epoch",
     "bos_token": "<s>",
     "strategy_token": "<strg>",
     "ref_token": "<refr>",
     "device": 'cuda:0',
 
-    "epochs": 40,
+    "epochs": 1,
     "batch_size": 32,
     "init_lr": 1e-4,
     "num_beams": 2, # only for evaluation (generate func.)
 
     "wandb": True,
-    "pretrained_path": "/workspace/positive_reframe/ckpt/bart-large-debug/best", # "/workspace/positive_reframe/ckpt/bart-large-finetune-3/best",
+    "pretrained_path": "/workspace/positive_reframe/ckpt/bart-large-one_epoch/best",
+    # "/workspace/positive_reframe/ckpt/bart-large-debug/best", # "/workspace/positive_reframe/ckpt/bart-large-finetune-3/best",
     "save_path": "/workspace/positive_reframe/ckpt" # leaf dir name will be run_name
     
 }
